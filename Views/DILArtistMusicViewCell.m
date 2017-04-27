77b5af88ac52aa8c927d754df979d01d33f9ec3e
meggrasse@u.northwestern.edu <meggrasse@gmail.com>
2017-04-27 16:44:00 -0500

    Cosmetic updates to external streaming assets, major refactoring.

--- Views/DILArtistMusicViewCell.m
+++ Views/DILArtistMusicViewCell.m
@@ -13,21 +13,19 @@
 #import <SVProgressHUD/SVProgressHUD.h>
 #import "ParseUI.h"
 
+@interface DILExternalStreamingServiceButton : UIButton
+@property (strong, nonatomic) NSURL *externalURL;
+@end
+
+@implementation DILExternalStreamingServiceButton
+@end
+
 @interface DILArtistMusicViewCell()
-@property (strong, nonatomic) UIButton *spotifyBtn;
-@property (strong, nonatomic) UIButton *tidalBtn;
-@property (strong, nonatomic) UIButton *aplBtn;
-@property (strong, nonatomic) UIButton *soundcloudBtn;
-@property (strong, nonatomic) UIButton *youtubeBtn;
-@property (strong, nonatomic) NSString *mainSpotifyUrl;
-@property (strong, nonatomic) NSString *mainTidalUrl;
-@property (strong, nonatomic) NSString *aplUrl;
-@property (strong, nonatomic) NSString *mainSoundcloudUrl;
-@property (strong, nonatomic) NSString *youtubeUrl;
-@property (strong, nonatomic) NSString *safeSpotifyUrl;
-@property (strong, nonatomic) NSString *safeTidalUrl;
-@property (strong, nonatomic) NSString *safeSoundcloudUrl;
-@property (strong, nonatomic) UIView *btnView;
+@property (strong, nonatomic) NSMutableArray *externalStreamingButtonArray;
+@property (strong, nonatomic) DILExternalStreamingServiceButton *spotifyLinkButton;
+@property (strong, nonatomic) DILExternalStreamingServiceButton *appleMusicLinkButton;
+@property (strong, nonatomic) DILExternalStreamingServiceButton *soundCloudLinkButton;
+@property (strong, nonatomic) DILExternalStreamingServiceButton *youtubeLinkButton;
 @end
 
 @implementation DILArtistMusicViewCell
@@ -39,168 +37,129 @@
 }
 
 - (void)configureCell {
-    CGFloat icon_dim = self.contentView.frame.size.width/4;
+    CGFloat iconSideLength = self.contentView.frame.size.width / 4;
+    CGFloat horizontalInset = self.contentView.frame.size.width / 16;
+    CGFloat verticalInset = 15.0;
+    CGFloat secondRowHorizontalInset = iconSideLength / 2 + horizontalInset;
     
-    [self.contentView addSubview:self.spotifyBtn];
-    [_spotifyBtn autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:20.0];
-    [_spotifyBtn autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:15.0];
-    [_spotifyBtn autoSetDimensionsToSize:CGSizeMake(icon_dim, icon_dim)];
-
-    [self.contentView addSubview:self.aplBtn];
-    [_aplBtn autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:20.0];
-    [_aplBtn autoAlignAxisToSuperviewAxis:ALAxisVertical];
-    [_aplBtn autoSetDimensionsToSize:CGSizeMake(icon_dim, icon_dim)];
+    CGSize iconSize = CGSizeMake(iconSideLength, iconSideLength);
     
-    [self.contentView addSubview:self.youtubeBtn];
-    [_youtubeBtn autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:20.0];
-    [_youtubeBtn autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:15.0];
-    [_youtubeBtn autoSetDimensionsToSize:CGSizeMake(icon_dim, icon_dim)];
+    [self.contentView addSubview:self.spotifyLinkButton];
+    [_spotifyLinkButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:verticalInset];
+    [_spotifyLinkButton autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:horizontalInset];
+    [_spotifyLinkButton autoSetDimensionsToSize:iconSize];
+
+    [self.contentView addSubview:self.appleMusicLinkButton];
+    [_appleMusicLinkButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:verticalInset];
+    [_appleMusicLinkButton autoAlignAxisToSuperviewAxis:ALAxisVertical];
+    [_appleMusicLinkButton autoSetDimensionsToSize:iconSize];
+
+    [self.contentView addSubview:self.youtubeLinkButton];
+    [_youtubeLinkButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:verticalInset];
+    [_youtubeLinkButton autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:horizontalInset];
+    [_youtubeLinkButton autoSetDimensionsToSize:iconSize];
+
     
-    
-    [self.contentView addSubview:self.soundcloudBtn];
-    [_soundcloudBtn autoSetDimensionsToSize:CGSizeMake(icon_dim, icon_dim)];
-    [_soundcloudBtn autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:_spotifyBtn];
-    [_soundcloudBtn autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:(icon_dim*.75)];
+    [self.contentView addSubview:self.soundCloudLinkButton];
+    [_soundCloudLinkButton autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:secondRowHorizontalInset];
+    [_soundCloudLinkButton autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:_spotifyLinkButton withOffset:verticalInset];
+    [_soundCloudLinkButton autoSetDimensionsToSize:iconSize];
 
 
-    [self.contentView addSubview:self.tidalBtn];
-    [_tidalBtn autoSetDimensionsToSize:CGSizeMake(icon_dim, icon_dim)];
-    [_tidalBtn autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:_youtubeBtn];
-    [_tidalBtn autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:(icon_dim*.75)];
- }
-
-- (UIButton *)spotifyBtn {
-    if (!_spotifyBtn) {
-        _spotifyBtn = [[UIButton alloc] initForAutoLayout];
-        UIImage *btnImage = [UIImage imageNamed:@"spotify"];;
-        [_spotifyBtn setBackgroundImage:btnImage forState:UIControlStateNormal];
-        [self.spotifyBtn addTarget:self action:@selector(openSpotify:) forControlEvents:UIControlEventTouchUpInside];
-    }
-    _spotifyBtn.userInteractionEnabled = YES;
-    return _spotifyBtn;
+//    [self.contentView addSubview:self.tidalBtn];
+//    [_tidalBtn autoSetDimensionsToSize:CGSizeMake(icon_dim, icon_dim)];
+//    [_tidalBtn autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:_youtubeLinkButton];
+//    [_tidalBtn autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:(icon_dim*.75)];
 }
 
-- (UIButton *)tidalBtn {
-    if (!_tidalBtn) {
-        _tidalBtn = [[UIButton alloc] initForAutoLayout];
-        UIImage *btnImage = [UIImage imageNamed:@"tidal"];
-        [_tidalBtn setBackgroundImage:btnImage forState:UIControlStateNormal];
-        _tidalBtn.frame = CGRectMake(200, 25, 150, 75);
-        [self.tidalBtn addTarget:self action:@selector(openTidal:) forControlEvents:UIControlEventTouchUpInside];
+- (DILExternalStreamingServiceButton *)spotifyLinkButton {
+    if (!_spotifyLinkButton) {
+        _spotifyLinkButton = [[DILExternalStreamingServiceButton alloc] initForAutoLayout];
+        [self.spotifyLinkButton addTarget:self action:@selector(openExternalStreamingPlaybackService:) forControlEvents:UIControlEventTouchUpInside];
     }
-    _tidalBtn.userInteractionEnabled = YES;
-    return _tidalBtn;
+    _spotifyLinkButton.userInteractionEnabled = YES;
+    return _spotifyLinkButton;
 }
 
-- (UIButton *)aplBtn {
-    if (!_aplBtn) {
-        _aplBtn = [[UIButton alloc] initForAutoLayout];
-        UIImage *btnImage = [UIImage imageNamed:@"applemusic"];
-        [_aplBtn setBackgroundImage:btnImage forState:UIControlStateNormal];
-        _aplBtn.frame = CGRectMake(10, 150, 150, 75);
-        [self.aplBtn addTarget:self action:@selector(openApl:) forControlEvents:UIControlEventTouchUpInside];
+//- (DILExternalStreamingServiceButton *)tidalBtn {
+//    if (!_tidalBtn) {
+//        _tidalBtn = [[UIButton alloc] initForAutoLayout];
+//        UIImage *btnImage = [UIImage imageNamed:@"tidal"];
+//        [_tidalBtn setBackgroundImage:btnImage forState:UIControlStateNormal];
+//        [self.tidalBtn addTarget:self action:@selector(openTidal:) forControlEvents:UIControlEventTouchUpInside];
+//    }
+//    _tidalBtn.userInteractionEnabled = YES;
+//    return _tidalBtn;
+//}
+
+- (DILExternalStreamingServiceButton *)appleMusicLinkButton {
+    if (!_appleMusicLinkButton) {
+        _appleMusicLinkButton = [[DILExternalStreamingServiceButton alloc] initForAutoLayout];
+        [self.appleMusicLinkButton addTarget:self action:@selector(openExternalStreamingPlaybackService:) forControlEvents:UIControlEventTouchUpInside];
     }
-    _aplBtn.userInteractionEnabled = YES;
-    return _aplBtn;
+    _appleMusicLinkButton.userInteractionEnabled = YES;
+    return _appleMusicLinkButton;
 }
 
-- (UIButton *)soundcloudBtn {
-    if (!_soundcloudBtn) {
-        _soundcloudBtn = [[UIButton alloc] initForAutoLayout];
-        UIImage *btnImage = [UIImage imageNamed:@"soundcloud"];
-        [_soundcloudBtn setBackgroundImage:btnImage forState:UIControlStateNormal];
-        _soundcloudBtn.frame = CGRectMake(200, 150, 150, 75);
-        [self.soundcloudBtn addTarget:self action:@selector(openSoundcloud:) forControlEvents:UIControlEventTouchUpInside];
+- (DILExternalStreamingServiceButton *)soundCloudLinkButton {
+    if (!_soundCloudLinkButton) {
+        _soundCloudLinkButton = [[DILExternalStreamingServiceButton alloc] initForAutoLayout];
+        [self.soundCloudLinkButton addTarget:self action:@selector(openExternalStreamingPlaybackService:) forControlEvents:UIControlEventTouchUpInside];
     }
-    _soundcloudBtn.userInteractionEnabled = YES;
-    return _soundcloudBtn;
+    _soundCloudLinkButton.userInteractionEnabled = YES;
+    return _soundCloudLinkButton;
 }
 
-- (UIButton *)youtubeBtn {
-    if (!_youtubeBtn) {
-        _youtubeBtn = [[UIButton alloc] initForAutoLayout];
-        UIImage *btnImage = [UIImage imageNamed:@"youtube"];
-        [_youtubeBtn setBackgroundImage:btnImage forState:UIControlStateNormal];
-        _youtubeBtn.frame = CGRectMake(10, 225, 150, 75);
-        [self.youtubeBtn addTarget:self action:@selector(openYoutube:) forControlEvents:UIControlEventTouchUpInside];
+- (DILExternalStreamingServiceButton *)youtubeLinkButton {
+    if (!_youtubeLinkButton) {
+        _youtubeLinkButton = [[DILExternalStreamingServiceButton alloc] initForAutoLayout];
+        [self.youtubeLinkButton addTarget:self action:@selector(openExternalStreamingPlaybackService:) forControlEvents:UIControlEventTouchUpInside];
     }
-    _youtubeBtn.userInteractionEnabled = YES;
-    return _youtubeBtn;
+    _youtubeLinkButton.userInteractionEnabled = YES;
+    return _youtubeLinkButton;
 }
 
 - (void)configureCellWithArtist:(DILPFArtist *)artist{
-    self.mainSpotifyUrl = [@"spotify://artist/" stringByAppendingString:artist.spotifyUrl];
-    self.mainTidalUrl = [@"tidal://artist/" stringByAppendingString:artist.tidalUrl];
-    self.aplUrl = [@"https://itun.es/us/" stringByAppendingString:artist.aplUrl]; // opens safari
-    self.mainSoundcloudUrl = [@"soundcloud:users:" stringByAppendingString:artist.soundcloudUrl];
-    self.youtubeUrl = [@"https://www.youtube.com/user/" stringByAppendingString:artist.youtubeUrl];
+    if (artist.spotifyUrl) {
+        NSURL *spotifyAppURL = [NSURL URLWithString:[@"spotify://artist/" stringByAppendingString:artist.spotifyUrl]];
+        NSURL *spotifyBackupURL = [NSURL URLWithString:[@"http://open.spotify.com/artist/" stringByAppendingString:artist.spotifyUrl]];
+        self.spotifyLinkButton.externalURL = [UIApplication.sharedApplication canOpenURL:spotifyAppURL] ? spotifyAppURL : spotifyBackupURL;
+        [_spotifyLinkButton setBackgroundImage:[UIImage imageNamed:@"SpotifyIcon"] forState:UIControlStateNormal];
+    } else {
+        [_spotifyLinkButton setBackgroundImage:[UIImage imageNamed:@"SpotifyIconBW"] forState:UIControlStateNormal];
+    }
     
-    self.safeSpotifyUrl = [@"http://open.spotify.com/artist/" stringByAppendingString:artist.spotifyUrl];
-    self.safeTidalUrl = [@"http://tidal.com/artist/" stringByAppendingString:artist.tidalUrl];
-    self.safeSoundcloudUrl = [@"https://soundcloud.com/" stringByAppendingString:artist.soundcloudUsername];
-}
-
-- (void)openSpotify:(id)sender {
-    if ([self.mainSpotifyUrl isEqualToString:@"spotify://artist/"]) {
-        [SVProgressHUD showInfoWithStatus:@"Artist not available on Spotify"];
-        return;
-    }
-    UIApplication *myApp = UIApplication.sharedApplication;
-    NSURL *realSpotifyUrl = [NSURL URLWithString: self.mainSpotifyUrl];
-    NSURL *fakeSpotifyUrl = [NSURL URLWithString: self.safeSpotifyUrl];
-    if ([myApp canOpenURL:realSpotifyUrl]) {
-        [myApp openURL:realSpotifyUrl];
+    if (artist.aplUrl) {
+        self.appleMusicLinkButton.externalURL = [NSURL URLWithString:[@"https://itun.es/us/" stringByAppendingString:artist.aplUrl]];
+        [_appleMusicLinkButton setBackgroundImage:[UIImage imageNamed:@"AppleMusicIcon"] forState:UIControlStateNormal];
     } else {
-        [myApp openURL:fakeSpotifyUrl];
+        [_appleMusicLinkButton setBackgroundImage:[UIImage imageNamed:@"AppleMusicIconBW"] forState:UIControlStateNormal];
     }
-}
-
-- (void)openTidal:(id)sender {
-    if ([self.mainTidalUrl isEqualToString:@"tidal://artist/"]) {
-        [SVProgressHUD showInfoWithStatus:@"Artist not available on Tidal"];
-        return;
-    }
-    UIApplication *myApp = UIApplication.sharedApplication;
-    NSURL *realTidalUrl = [NSURL URLWithString: self.mainTidalUrl];
-    NSURL *fakeTidalUrl = [NSURL URLWithString: self.safeTidalUrl];
-    if ([myApp canOpenURL:realTidalUrl]) {
-        [myApp openURL:realTidalUrl];
+    
+    if (artist.soundcloudUrl) {
+        NSURL *soundCloudAppURL = [NSURL URLWithString:[@"soundcloud:users:" stringByAppendingString:artist.soundcloudUrl]];
+        NSURL *soundCloudBackupURL = [NSURL URLWithString:[@"https://soundcloud.com/" stringByAppendingString:artist.soundcloudUrl]];
+        self.soundCloudLinkButton.externalURL = [UIApplication.sharedApplication canOpenURL:soundCloudAppURL] ? soundCloudAppURL : soundCloudBackupURL;
+        [_soundCloudLinkButton setBackgroundImage:[UIImage imageNamed:@"SoundCloudIcon"] forState:UIControlStateNormal];
     } else {
-        [myApp openURL:fakeTidalUrl];
+        [_soundCloudLinkButton setBackgroundImage:[UIImage imageNamed:@"SoundCloudIconBW"] forState:UIControlStateNormal];
     }
-}
-
-- (void)openApl:(id)sender {
-    if ([self.aplUrl isEqualToString:@"https://itun.es/us/"]) {
-        [SVProgressHUD showInfoWithStatus:@"Artist not available on iTunes"];
-        return;
-    }
-    [[UIApplication sharedApplication] openURL:
-     [NSURL URLWithString: self.aplUrl]];
-}
-
-- (void)openSoundcloud:(id)sender {
-    if ([self.mainSoundcloudUrl isEqualToString:@"soundcloud:users:"]) {
-        [SVProgressHUD showInfoWithStatus:@"Artist not available on Soundcloud"];
-        return;
-    }
-    UIApplication *myApp = UIApplication.sharedApplication;
-    NSURL *realSoundcloudUrl = [NSURL URLWithString: self.mainSoundcloudUrl];
-    NSURL *fakeSoundcloudUrl = [NSURL URLWithString: self.safeSoundcloudUrl];
-    if ([myApp canOpenURL:realSoundcloudUrl]) {
-        [myApp openURL:realSoundcloudUrl];
+    
+    if (artist.youtubeUrl) {
+        self.youtubeLinkButton.externalURL = [NSURL URLWithString:[@"https://www.youtube.com/user/" stringByAppendingString:artist.youtubeUrl]];
+        [_youtubeLinkButton setBackgroundImage:[UIImage imageNamed:@"YoutubeIcon"] forState:UIControlStateNormal];
     } else {
-        [myApp openURL:fakeSoundcloudUrl];
+        [_youtubeLinkButton setBackgroundImage:[UIImage imageNamed:@"YoutubeIconBW"] forState:UIControlStateNormal];
     }
 }
-
-- (void)openYoutube:(id)sender {
-    if ([self.youtubeUrl isEqualToString:@"https://www.youtube.com/user/"]) {
-        [SVProgressHUD showInfoWithStatus:@"Artist not available on Youtube"];
-        return;
+                                                              
+- (void)openExternalStreamingPlaybackService:(id)sender {
+    DILExternalStreamingServiceButton *currentButton = (DILExternalStreamingServiceButton *)sender;
+    if (currentButton.externalURL) {
+        [UIApplication.sharedApplication openURL:currentButton.externalURL];
+    } else {
+        [SVProgressHUD showInfoWithStatus:@"Artist hasn't shared their music here yet"];
     }
-    [[UIApplication sharedApplication] openURL:
-     [NSURL URLWithString: self.youtubeUrl]];
 }
 
 + (NSString *)identifier {
